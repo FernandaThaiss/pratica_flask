4e33a171a7c6b1b7571fd1f91265d21984373281
Yasmin Fernandes <132394700+Yasminsz@users.noreply.github.com>
2024-10-21 10:11:04 -0300

    aulaParte2

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app= Flask (__name__)
+
 
 @app.route("/")
 def index ():
     return "<h1>Olá, Mundo!</h1>"
 
-@app.route("/aluno/<nome>")
-def aluno(nome):
-    return nome
+@app.route("/aluno")
+def aluno():
+    return render_template('formulario.html')
+
+@app.route("/envio", methods=['POST'])
+def envioForms():
+    nombre = request.form['nome']
+    senha = request.form['senha']
+    return render_template('aluno.html', n=nombre, s=senha)
+   
